8636e4e0def11f923f1a9cda643888ad6dc61312
aditi-bhaskar <44757648+aditi-bhaskar@users.noreply.github.com>
2022-01-18 11:00:20 -0800

    added choice to play multiple games

--- src/app/App.java
+++ src/app/App.java
@@ -1,21 +1,46 @@
 package app;
+import java.util.*
 
 public class App {
 
     static board tictactoe = new board();
     public static void main(String[] args) throws Exception {
-        System.out.println("TicTacToe Game");
         
-        player p1 = new humanplayer(tictactoe);
-        player p2 = new computerplayer(tictactoe);
+        Scanner s = new Scanner(System.in) ;
+        int game = -1 ;
+        
+        while (game < 0 && game > 1)  { // change limits as more games are added
+            System.out.println("Which game do you want to play? (enter the number)")
+            System.out.println("\t 0 - TicTacToe")
+            System.out.println("\t 1 - StoryBuilder")
 
-        tictactoe.printboard();
+            game = s.nextInt() ;
 
-        game g = new game(p1, p2, tictactoe);
+            if (game == 0) { // TICTACTOE
+                System.out.println("TicTacToe Game");
 
-        System.out.println(g.playgame());
+                player p1 = new humanplayer(tictactoe);
+                player p2 = new computerplayer(tictactoe);
 
-    /*
+                tictactoe.printboard();
+
+                game g = new game(p1, p2, tictactoe);
+
+                System.out.println(g.playgame()); 
+                
+//                 return;
+            } 
+            else if(game == 1) { // STORYBUILDER
+                System.out.println("StoryBuilder Game");
+                
+//                 return;
+            }
+        
+        }
+        
+
+
+    /* DEAD CODE!!!!!!!!!!
         while (tictactoe.checkwin() == false){
 
             //Human Play
